ca169a064b92bb43c8e52b67acb880888f24c351
Kolmoskanava <frans.korpela123@hotmail.com>
2026-08-21 17:59:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const io = new Server(server, {
 });
 
 const activeUsers = {};
-const inCallUsers = new Set(); // Seurataan varattuja numeroita
+const inCallUsers = new Set();
 
 io.on('connection', (socket) => {
 
@@ -23,14 +23,12 @@ io.on('connection', (socket) => {
   socket.on('start_call', (data) => {
     const targetSocketId = activeUsers[data.targetNumber];
     
-    // TARKISTUS: Jos kohde on jo puhelussa
     if (inCallUsers.has(data.targetNumber)) {
       socket.emit('line_busy');
       return;
     }
 
     if (targetSocketId) {
-      // Merkitään soittaja puhelutilaan myöhempiä tarkistuksia varten
       inCallUsers.add(data.fromNumber);
       
       io.to(targetSocketId).emit('incoming_call', { 
@@ -38,13 +36,11 @@ io.on('connection', (socket) => {
         channel: data.channel 
       });
     } else {
-      // Jos numeroa ei löydy linjoilta, lähetetään myös varattu-tieto
       socket.emit('line_busy');
     }
   });
 
   socket.on('answer_call', (data) => {
-    // Merkitään myös vastaaja varatuksi kun puhelu yhdistyy
     for (const [num, id] of Object.entries(activeUsers)) {
       if (id === socket.id) inCallUsers.add(num);
     }
@@ -55,15 +51,30 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('audio_stream', (data) => {
+  // --- WEBRTC SIGNALISOINTI (Välittää yhteydenmuodostusviestit) ---
+  socket.on('webrtc_offer', (data) => {
     const targetSocketId = activeUsers[data.targetNumber];
     if (targetSocketId) {
-      io.to(targetSocketId).emit('receive_audio', data.audioChunk);
+      io.to(targetSocketId).emit('webrtc_offer', { fromNumber: data.fromNumber, offer: data.offer });
     }
   });
 
+  socket.on('webrtc_answer', (data) => {
+    const targetSocketId = activeUsers[data.targetNumber];
+    if (targetSocketId) {
+      io.to(targetSocketId).emit('webrtc_answer', { answer: data.answer });
+    }
+  });
+
+  socket.on('webrtc_ice_candidate', (data) => {
+    const targetSocketId = activeUsers[data.targetNumber];
+    if (targetSocketId) {
+      io.to(targetSocketId).emit('webrtc_ice_candidate', { candidate: data.candidate });
+    }
+  });
+  // -------------------------------------------------------------
+
   socket.on('end_call', (data) => {
-    // Vapautetaan molemmat numerot puhelun päättyessä
     for (const [num, id] of Object.entries(activeUsers)) {
       if (id === socket.id) inCallUsers.delete(num);
     }
