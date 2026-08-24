5f07af8c9ea1a2cbd81da8afdb50a3329faa46e2
Kolmoskanava <frans.korpela123@hotmail.com>
2026-08-24 08:00:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-// Lisätty reitti juuriosoitteelle, jotta fetch-pyynnöt eivät anna 404-virhettä
+// Muutettu juurireitti palauttamaan myös IP-osoitteen JSON-muodossa
 app.get('/', (req, res) => {
-  res.status(200).send('Mobira Server Online');
+    // Haetaan IP (Renderin ja muiden proxyn takana x-forwarded-for on luotettavin)
+    const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    
+    res.status(200).json({
+        status: 'Mobira Server Online',
+        ip: clientIp
+    });
 });
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
+    cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
 const activeUsers = {};
@@ -21,95 +27,94 @@ const inCallUsers = new Set();
 
 io.on('connection', (socket) => {
 
-  socket.on('register_number', (number) => {
-    activeUsers[number] = socket.id;
-  });
+    socket.on('register_number', (number) => {
+        activeUsers[number] = socket.id;
+    });
 
-  socket.on('start_call', (data) => {
-    const targetSocketId = activeUsers[data.targetNumber];
-    
-    // Tarkistetaan, että kumpikaan osapuoli ei ole jo varattu
-    if (inCallUsers.has(data.targetNumber) || inCallUsers.has(data.fromNumber)) {
-      socket.emit('line_busy');
-      return;
-    }
+    socket.on('start_call', (data) => {
+        const targetSocketId = activeUsers[data.targetNumber];
+        
+        // Tarkistetaan, että kumpikaan osapuoli ei ole jo varattu
+        if (inCallUsers.has(data.targetNumber) || inCallUsers.has(data.fromNumber)) {
+            socket.emit('line_busy');
+            return;
+        }
 
-    if (targetSocketId) {
-      // Merkitään molemmat osapuolet varatuiksi
-      inCallUsers.add(data.fromNumber);
-      inCallUsers.add(data.targetNumber);
-      
-      io.to(targetSocketId).emit('incoming_call', { 
-        fromNumber: data.fromNumber,
-        channel: data.channel 
-      });
-    } else {
-      socket.emit('line_busy');
-    }
-  });
+        if (targetSocketId) {
+            // Merkitään molemmat osapuolet varatuiksi
+            inCallUsers.add(data.fromNumber);
+            inCallUsers.add(data.targetNumber);
+            
+            io.to(targetSocketId).emit('incoming_call', { 
+                fromNumber: data.fromNumber,
+                channel: data.channel 
+            });
+        } else {
+            socket.emit('line_busy');
+        }
+    });
 
-  socket.on('answer_call', (data) => {
-    for (const [num, id] of Object.entries(activeUsers)) {
-      if (id === socket.id) inCallUsers.add(num);
-    }
-    
-    const callerSocketId = activeUsers[data.targetNumber];
-    if (callerSocketId) {
-      io.to(callerSocketId).emit('call_answered');
-    }
-  });
+    socket.on('answer_call', (data) => {
+        for (const [num, id] of Object.entries(activeUsers)) {
+            if (id === socket.id) inCallUsers.add(num);
+        }
+        
+        const callerSocketId = activeUsers[data.targetNumber];
+        if (callerSocketId) {
+            io.to(callerSocketId).emit('call_answered');
+        }
+    });
 
-  // --- WEBRTC SIGNALISOINTI ---
-  socket.on('webrtc_offer', (data) => {
-    const targetSocketId = activeUsers[data.targetNumber];
-    if (targetSocketId) {
-      io.to(targetSocketId).emit('webrtc_offer', { fromNumber: data.fromNumber, offer: data.offer });
-    }
-  });
+    // --- WEBRTC SIGNALISOINTI ---
+    socket.on('webrtc_offer', (data) => {
+        const targetSocketId = activeUsers[data.targetNumber];
+        if (targetSocketId) {
+            io.to(targetSocketId).emit('webrtc_offer', { fromNumber: data.fromNumber, offer: data.offer });
+        }
+    });
 
-  socket.on('webrtc_answer', (data) => {
-    const targetSocketId = activeUsers[data.targetNumber];
-    if (targetSocketId) {
-      io.to(targetSocketId).emit('webrtc_answer', { answer: data.answer });
-    }
-  });
+    socket.on('webrtc_answer', (data) => {
+        const targetSocketId = activeUsers[data.targetNumber];
+        if (targetSocketId) {
+            io.to(targetSocketId).emit('webrtc_answer', { answer: data.answer });
+        }
+    });
 
-  socket.on('webrtc_ice_candidate', (data) => {
-    const targetSocketId = activeUsers[data.targetNumber];
-    if (targetSocketId) {
-      io.to(targetSocketId).emit('webrtc_ice_candidate', { candidate: data.candidate });
-    }
-  });
-  // -----------------------------
+    socket.on('webrtc_ice_candidate', (data) => {
+        const targetSocketId = activeUsers[data.targetNumber];
+        if (targetSocketId) {
+            io.to(targetSocketId).emit('webrtc_ice_candidate', { candidate: data.candidate });
+        }
+    });
+    // -----------------------------
 
-  socket.on('end_call', (data) => {
-    // Vapautetaan kaikki socketiin tai targetNumberiin liittyvät numerot varatusta tilasta
-    for (const [num, id] of Object.entries(activeUsers)) {
-      if (id === socket.id) {
-        inCallUsers.delete(num);
-      }
-    }
-    if (data && data.targetNumber) {
-      inCallUsers.delete(data.targetNumber);
-      const targetSocketId = activeUsers[data.targetNumber];
-      if (targetSocketId) {
-        io.to(targetSocketId).emit('call_ended');
-      }
-    }
-  });
+    socket.on('end_call', (data) => {
+        for (const [num, id] of Object.entries(activeUsers)) {
+            if (id === socket.id) {
+                inCallUsers.delete(num);
+            }
+        }
+        if (data && data.targetNumber) {
+            inCallUsers.delete(data.targetNumber);
+            const targetSocketId = activeUsers[data.targetNumber];
+            if (targetSocketId) {
+                io.to(targetSocketId).emit('call_ended');
+            }
+        }
+    });
 
-  socket.on('disconnect', () => {
-    for (const [number, id] of Object.entries(activeUsers)) {
-      if (id === socket.id) {
-        delete activeUsers[number];
-        inCallUsers.delete(number);
-        break;
-      }
-    }
-  });
+    socket.on('disconnect', () => {
+        for (const [number, id] of Object.entries(activeUsers)) {
+            if (id === socket.id) {
+                delete activeUsers[number];
+                inCallUsers.delete(number);
+                break;
+            }
+        }
+    });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Mobira-palvelin pyörii portissa ${PORT}`);
+    console.log(`Mobira-palvelin pyörii portissa ${PORT}`);
 });
